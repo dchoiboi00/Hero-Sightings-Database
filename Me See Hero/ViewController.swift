e9d46f316eb33b623d703b8a01a134cae3d440af
Yoo Choi <dchoiboi00@gmail.com>
2019-10-31 14:12:20 -0400

    Initial Commit

---
+++ Me See Hero/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Me See Hero
+//
+//  Created by Daniel Choi on 10/31/19.
+//  Copyright © 2019 Daniel Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
