06030705d12caa8a4c6eb7db919384bbb615812a
Yoo Choi <dchoiboi00@gmail.com>
2019-10-31 18:24:57 -0400

    Added view controllers

---
+++ Me See Hero/Classes/HeroTVCell.swift
@@ -0,0 +1,24 @@
+//
+//  HeroTVCell.swift
+//  Me See Hero
+//
+//  Created by Daniel Choi on 10/31/19.
+//  Copyright © 2019 Daniel Choi. All rights reserved.
+//
+
+import UIKit
+
+class HeroTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
