917d2dcfb7f035de3226fccb962603397f27b26a
Yoo Choi <dchoiboi00@gmail.com>
2019-10-31 19:27:52 -0400

    Changed the AddHeroVC as a modal form, centered in the middle

--- Me See Hero/CoreData/CoreDataStack.swift
+++ Me See Hero/CoreData/CoreDataStack.swift
@@ -12,73 +12,73 @@ import CoreData
 final class CoreDataStack: NSObject {
     
     static let shared = CoreDataStack()
-    
-    let modelName = "MediaTracker"
-    
-    lazy var context: NSManagedObjectContext = {
-        return self.persistentContainer.viewContext
-    }()
-    
-    lazy var persistentContainer: NSPersistentContainer = {
-        
-        let container = NSPersistentContainer(name: "MediaTracker")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-    
-    var items: [NSManagedObject] = []
-    
-    // MARK: - Core Data operations
-    
-    func update() {
-        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Item")
-        do {
-            items = try context.fetch(fetchRequest)
-        } catch let error as NSError {
-            print("Could not fetch requested item. \(error), \(error.userInfo)")
-        }
-    }
-    
-    func saveItem(title: String, artist: String) {
-        if let entity = NSEntityDescription.entity(forEntityName: "Item", in: context) {
-            let item = NSManagedObject(entity: entity, insertInto: context)
-            item.setValue(title, forKeyPath: "title")
-            item.setValue(artist, forKeyPath: "artist")
-            do {
-                try context.save()
-            } catch let error as NSError {
-                print("Could not save the item. \(error), \(error.userInfo)")
-            }
-        }
-        update()
-    }
-    
-    func deleteItem(item: Item) {
-        if let _ = items.firstIndex(of: item)  {
-            context.delete(item)
-            do {
-                try context.save()
-            } catch let error as NSError {
-                print("Could not delete the item. \(error), \(error.userInfo)")
-            }
-        }
-        update()
-    }
-    
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
-    
+//
+//    let modelName = "MediaTracker"
+//
+//    lazy var context: NSManagedObjectContext = {
+//        return self.persistentContainer.viewContext
+//    }()
+//
+//    lazy var persistentContainer: NSPersistentContainer = {
+//
+//        let container = NSPersistentContainer(name: "MediaTracker")
+//        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+//            if let error = error as NSError? {
+//                fatalError("Unresolved error \(error), \(error.userInfo)")
+//            }
+//        })
+//        return container
+//    }()
+//
+//    var items: [NSManagedObject] = []
+//
+//    // MARK: - Core Data operations
+//
+//    func update() {
+//        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Item")
+//        do {
+//            items = try context.fetch(fetchRequest)
+//        } catch let error as NSError {
+//            print("Could not fetch requested item. \(error), \(error.userInfo)")
+//        }
+//    }
+//
+//    func saveItem(title: String, artist: String) {
+//        if let entity = NSEntityDescription.entity(forEntityName: "Item", in: context) {
+//            let item = NSManagedObject(entity: entity, insertInto: context)
+//            item.setValue(title, forKeyPath: "title")
+//            item.setValue(artist, forKeyPath: "artist")
+//            do {
+//                try context.save()
+//            } catch let error as NSError {
+//                print("Could not save the item. \(error), \(error.userInfo)")
+//            }
+//        }
+//        update()
+//    }
+//
+//    func deleteItem(item: Item) {
+//        if let _ = items.firstIndex(of: item)  {
+//            context.delete(item)
+//            do {
+//                try context.save()
+//            } catch let error as NSError {
+//                print("Could not delete the item. \(error), \(error.userInfo)")
+//            }
+//        }
+//        update()
+//    }
+//
+//    func saveContext () {
+//        let context = persistentContainer.viewContext
+//        if context.hasChanges {
+//            do {
+//                try context.save()
+//            } catch {
+//                let nserror = error as NSError
+//                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+//            }
+//        }
+//    }
+//
 }
